c05c5139ef8243c819e1142ff206c3198866cda7
Akashdip Neogi <Akashdipneogi@gmail.com>
2025-01-17 17:52:29 +0530

    Added solution approach to the code

--- Arrays/108. Convert Sorted Array to Binart Search Tree.cpp
+++ Arrays/108. Convert Sorted Array to Binart Search Tree.cpp
@@ -1,21 +1,29 @@
 /*
     https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
 */
+/*
+    The question asks for, balanced binary tree,
+        which means both the right & left side of the node should have values in it.
 
-/****************************************************************************/
+    Ex:-
+        nums = [-10,-3,0,5,9]
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+        The binary tree will be,
 
+                    0
+                   / \
+                 -3   5
+                 /   /
+               -10  9
+
+        The head is the mid element of the array, and
+            the left and right child are the mid elements of the left and right
+                subarray respectively.
+
+    -----------------------------------------------------------------------------
+    * Helper function to pass the values, the left and right index of the array.
+    * Storing the left & right values of the array on the left & rightside of the mid element.
+*/
 class Solution {
 public:
     TreeNode* addNode(vector<int> a, int left, int right){
@@ -26,10 +34,11 @@ public:
         TreeNode * temp = new TreeNode(a[mid]);
 
         temp->left = addNode(a, left, mid - 1);
-        temp->right = addNode(a, mid+1, right);
+        temp->right = addNode(a, mid + 1, right);
 
         return temp;
     }
+
     TreeNode* sortedArrayToBST(vector<int>& nums) {
         return addNode(nums, 0, nums.size() - 1);
     }
