a6bd42816cb70ee697e1a2f21fd2f2dcbb82e8c3
Akashdip Neogi <Akashdipneogi@gmail.com>
2025-01-18 01:21:38 +0530

    Added solution approach to the code and updated the code

--- Arrays/118. Pascal's Triangle.cpp
+++ Arrays/118. Pascal's Triangle.cpp
@@ -1,26 +1,38 @@
 /*
     https://leetcode.com/problems/pascals-triangle/
 */
+/*
+    Solution:
+
+    1. Initialize a 2D vector triangle with the first row as {1}.
+    2. Iterate from 1 to numRows.
+    3. Create a new vector row.
+    4. Push 1 to the row.
+    5. Iterate from 1 to prevRow.size() - 1.
+    6. Push prevRow[j - 1] + prevRow[j] to the row.
+    7. Push 1 to the row.
+    8. Push the row to the triangle.
+    9. Return the triangle.
+*/
 
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> a;
-        a.push_back({1});
+        vector<vector<int>> triangle;
+        triangle.push_back({1});
 
-        if(numRows == 1)
-            return a;
+        for (int i = 1; i < numRows; i++) {
+            vector<int> row;
+            vector<int>& prevRow = triangle[i - 1];
+            row.push_back(1);
 
-        a.push_back({1,1});
-        if(numRows == 2)
-            return a;
-        
-        for (int i = 2; i < numRows; i++){
-           vector<int> b(i+1, 1);
-           for(int j = 1; j < i; j++)
-               b[j] = a[i - 1][j] + a[i - 1][j - 1];
-            a.push_back(b);
+            for (int j = 1; j < prevRow.size(); j++)
+                row.push_back(prevRow[j - 1] + prevRow[j]);
+
+            row.push_back(1);
+            triangle.push_back(row);
         }
-        return a;
+
+        return triangle;
     }
 };
