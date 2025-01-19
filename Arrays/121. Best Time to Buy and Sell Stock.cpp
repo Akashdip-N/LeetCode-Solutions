28f8472c4b3792e75e3706e262550beb1c543ed2
Akashdip Neogi <Akashdipneogi@gmail.com>
2025-01-19 21:16:53 +0530

    Added solution approach to the code and updated the code

--- Arrays/121. Best Time to Buy and Sell Stock.cpp
+++ Arrays/121. Best Time to Buy and Sell Stock.cpp
@@ -1,20 +1,40 @@
 /*
     https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 */
+/*
+    Solution Approach:-
+
+    Ex:- [7,1,5,3,6,4]
+    Ans:- 5, becasue the best time to buy is 1 and best time to sell is 6,
+             so the profit is 6-1 = 5
+
+    To implement this solution,
+
+    1. Initialize min = prices[0] and profit = 0
+    2. Traverse the array from 1 to n
+        a. If min > prices[i], which means the current price is less than the min,
+            then update min = prices[i]
+
+        b. Else, if profit < (prices[i] - min),
+            which means that the
+                current profit is more than the difference between the current price and min,
+            then update profit = prices[i] - min
+    3. Return profit
+*/
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        
-        int mintilnow = prices[0];
+        int min = prices[0];
+        int profit = 0;
 
-        int profit = INT_MIN;
-
-        for(int i = 0; i < prices.size(); i++){
-            if(mintilnow > prices[i])
-                mintilnow = prices[i];
-            if(prices[i] - mintilnow > profit)
-                profit = prices[i] - mintilnow;
+        for(int i = 1; i < prices.size(); i++){
+            if(min > prices[i])
+                min = prices[i];
+            else
+                if(profit < (prices[i] - min))
+                    profit = prices[i] - min;
         }
+
         return profit;
     }
 };
--- Arrays/169. Majority Element.cpp
+++ Arrays/169. Majority Element.cpp
@@ -1,10 +1,69 @@
 /*
     https://leetcode.com/problems/majority-element/
 */
+/*
+    Boyer-Moore Voting Algorithm Solution Approach:-
+
+    To implement this solution,
+    1. Initialize ans = nums[0] and freq = 0
+    2. Traverse the array
+        a. If freq == 0, which means the frequency of the current element is 0,
+            then update ans = nums[i]
+        b. If ans == nums[i], which means the current element is the majority element,
+            then increment freq
+        c. Else, decrement freq
+    3. Return ans
+*/
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        return nums[(nums.size()-1)/2];
+        int ans = nums[0], freq = 0;
+        for (int i = 0; i < nums.size(); i++){
+            if (freq == 0)
+                ans = nums[i];
+
+            if (ans == nums[i])
+                freq++;
+            else
+                freq--;
+        }
+
+        return ans;
+    }
+};
+
+/*************************************************************************/
+
+/*
+    Map Implementation Solution Approach:-
+
+    To implement this solution:-
+    1. Initialize an unordered_map<int, int> mp
+    2. Traverse the array
+        a. If the element is not present in the map,
+            then add the element to the map with value 1
+        b. Else, increment the value of the element in the map
+        c. If the value of the element in the map is greater than n/2,
+            where n is the size of the array, then return the element
+    3. Return 0
+*/
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> mp;
+
+        if(nums.size() == 1)
+            return nums[0];
+
+        for(auto num : nums)
+            if(mp.find(num) == mp.end())
+                mp[num] = 1;
+            else{
+                mp[num]++;
+                if(mp[num] > nums.size()/2)
+                    return num;
+            }
+
+        return 0;
     }
 };
