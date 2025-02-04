de8eb5417ab9dd1eed880ae7c3b9523948800ebd
Akashdip Neogi <Akashdipneogi@gmail.com>
2025-02-04 22:48:17 +0000

    Solved problem 14. Longest Common prefix

--- Strings/Easy/14. Longest Common prefix.cpp
+++ Strings/Easy/14. Longest Common prefix.cpp
@@ -1,43 +1,24 @@
 /*
     https://leetcode.com/problems/longest-common-prefix/
+
+    Approach:-
+    * We will check with for all the characters in the first string, to the other strings.
 */
+
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string a;
-        bool ch = false;
-        int n = strs.size();
-        sort(strs.begin() ,strs.begin() + n);
-        int size = strs.size();
-        for (int i = 0; i < strs[1].size(); i++)
-        {
-            for (int j = 1; i < size; j++)
-                if (strs[1][i] == strs[j][i]) // I was having an error of segmenatation 
-                    ch = true; 
-                else
-                    ch = false;
+        string prefix = "";
 
-            if(ch)
-                a.push_back(strs[1][i]);
-            else
-                break;
+        for(int i = 0; i < strs[0].length(); i++){
+            char check = strs[0][i];
+
+            for(int j = 1; j < strs.size(); j++) // comparing with other strings
+                if(check != strs[j][i])          // Comparing the character
+                    return prefix;
+
+            prefix += check; // Add the character to the prefix
         }
-
-        return a;
-    }
-};
-
-/*-----------------------------------------------------------------------------
-            SOLUTION ON THE INTERNET
-*/
-
-class Solution {
-public:
-    string longestCommonPrefix(vector<string> &strs) {
-        int i, j, n = strs.size();
-        if (n == 0) return "";
-        sort(strs.begin() ,strs.begin() + n);
-        for (j = 0; j < strs[0].size() && j < strs[n - 1].size() && strs[0][j] == strs[n - 1][j]; j++);
-        return strs[0].substr(0, j);
+        return prefix;
     }
 };
