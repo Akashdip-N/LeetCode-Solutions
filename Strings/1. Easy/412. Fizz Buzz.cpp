bb560fecca0964e5aa8cf0ac42af58770cad4747
Akashdip Neogi <Akashdipneogi@gmail.com>
2025-02-17 10:18:57 +0000

    412. Fizz Buzz, Better solution

--- Strings/1. Easy/412. Fizz Buzz.cpp
+++ Strings/1. Easy/412. Fizz Buzz.cpp
@@ -15,17 +15,17 @@ public:
     vector<string> fizzBuzz(int n) {
         vector<string> s;
         for (int i = 1; i <= n; i++)
-            if((i % 3 == 0)&&(i % 5 == 0))
-                s.push_back("FizzBuzz");
-            else
-                if(i % 3 == 0)
-                s.push_back("Fizz");
+            if(i % 3 == 0){
+                if(i % 5 == 0)
+                    s.push_back("FizzBuzz");
+                else
+                    s.push_back("Fizz");
+            }
             else
                 if(i % 5 == 0)
-                s.push_back("Buzz");
+                    s.push_back("Buzz");
             else
-                    s.push_back(to_string(i));
-
+                s.push_back(to_string(i));
         return s;
     }
 };
