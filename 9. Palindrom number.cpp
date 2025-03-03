9c6eb9337f4bb4a199affe47fdd6b64520cad564
Akashdip Neogi <Akashdipneogi@gmail.com>
2025-03-03 20:07:58 +0000

    Added solution explanation

--- 9. Palindrom number.cpp
+++ 9. Palindrom number.cpp
@@ -1,42 +1,36 @@
 /*
     https://leetcode.com/problems/palindrome-number/
 */
+/*
+    Time Complexity: O(log10(n))
+    Space Complexity: O(1)
+
+    Solution Approach:-
+    * We are first trying to get the length of the number,
+        and along with that what's the greatest power of 10 that is less than the number.
+
+    * Running a loop to check if the first and last digits are equal,
+        and reducing the number by removing the first and last digits.
+
+    * Reducing the div by 100 in each iteration to remove the first and last digits.
+*/
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x < 0) return false;
-        int temp = x;
-        long long int rev = 0;
+        if (x < 0) return false;
+
+        long div = 1;
+        while(x >= (div*10))
+            div *= 10;
 
         while(x > 0){
-            rev = rev * 10 + x % 10;
-            x /= 10;
-        }
-
-        return temp == rev;
-    }
-};
-
-/*                      USING STACK                  */
-/*                    BAD TIME COMPLEXITY            */
-class Solution {
-public:
-    bool isPalindrome(int x) {
-        if(x < 0) return false;
-        stack<int> s;
-        int temp = x;
-        while(x > 0){
-            s.push(x % 10);
-            x /= 10;
-        }
-
-        while(!s.empty()){
-            if(s.top() != temp % 10)
+            if ((x / div) != (x % 10))
                 return false;
 
-            s.pop();
-            temp /= 10;
+            x = (x % div) / 10; // remove the first and last digits
+            div /= 100;
         }
+
         return true;
     }
 };
