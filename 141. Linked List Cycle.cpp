4c739cd9a9b226f26f910fb54633969b16337a89
Akashdip Neogi <Akashdipneogi@gmail.com>
2025-03-10 23:44:38 +0000

    Solvedproblem 141

--- 141. Linked List Cycle.cpp
+++ 141. Linked List Cycle.cpp
@@ -1,14 +1,21 @@
 /*
     https://leetcode.com/problems/linked-list-cycle/
 */
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+/*
+    Solution Approach: Floyd's Cycle Detection Algorithm
+
+    Floyd's Cycle Detection Algorithm is a two-pointer approach
+        that uses a slow pointer and a fast pointer
+            to detect cycles in a linked list.
+    The slow pointer moves one step at a time,
+        while the fast pointer moves two steps at a time.
+    If there is a cycle in the linked list,
+        the fast pointer will eventually catch up to the slow pointer.
+
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+*/
+
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
@@ -24,6 +31,39 @@ public:
 
             if (slow == fast)
                 return true;
+        }
+
+        return false;
+    }
+};
+
+/*****************************************************************/
+/*
+    Solution Approach: Hash Table
+
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+
+    We can use a hash table to keep track of the nodes we have visited.
+    If we encounter a node that is already in the hash table,
+        then we know that there is a cycle in the linked list.
+
+*/
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head == nullptr || head->next == nullptr)
+            return false;
+
+        unordered_map<ListNode*, int> visited;
+        ListNode* curr = head;
+
+        while(curr != nullptr) {
+            if(visited.find(curr) != visited.end())
+                return true;
+
+            visited[curr] = 1;
+            curr = curr->next;
         }
 
         return false;
