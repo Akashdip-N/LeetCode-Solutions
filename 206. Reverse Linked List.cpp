be5675452f6966ea3f0bb3ed3135250781f11471
Akashdip Neogi <Akashdipneogi@gmail.com>
2025-03-13 08:31:02 +0000

    Added solution explanation and better solution

--- 206. Reverse Linked List.cpp
+++ 206. Reverse Linked List.cpp
@@ -1,33 +1,52 @@
 /*
     https://leetcode.com/problems/reverse-linked-list/
 */
+/*
+    Solution Approach:- Two pointers approach
+    Time Complexity: O(n)
+    Space Complexity: O(1)
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+    Taking two pointers prev and next and reversing the links between nodes.
+*/
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         if (head == NULL)
-		return nullptr;
-        else{
-            ListNode *current = head;
-            ListNode *prev = NULL, *next = NULL;
-            while (current != NULL){
-                next = current->next;
-                current->next = prev;
-                prev = current;
-                current = next;
-            }
-            head = prev;
-            return head;
+            return nullptr;
+
+		ListNode *current = head;
+        ListNode *prev = NULL, *next = NULL;
+
+        while (current != NULL){
+            next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
         }
+
+        head = prev;
+        return head;
+    }
+};
+
+/****************************************************** */
+/*
+    Solution Approach:- Recursive approach
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+
+*/
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == NULL || head->next == NULL) // base case
+            return head;
+
+        ListNode *newHead = reverseList(head->next);
+
+        head->next->next = head;
+        head->next = NULL;
+
+        return newHead;
     }
 };
