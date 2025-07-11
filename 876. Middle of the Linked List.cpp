077330de27cc2365def4b7e786638440e5d30d35
Akashdip Neogi <Akashdipneogi@gmail.com>
2025-07-11 20:40:36 +0100

    Solved problem number 876

---
+++ 876. Middle of the Linked List.cpp
@@ -0,0 +1,31 @@
+/*
+    https://leetcode.com/problems/middle-of-the-linked-list/description/
+*/
+/*
+    Solution Approach: Using Floyd's cycle detection algorithm (Two Pointers Approach)
+
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+
+    Explanation:
+        - Initialize two pointers, slow and fast, both pointing to the head.
+        - Move the slow pointer one step at a time and
+            the fast pointer two steps at a time.
+        - When the fast pointer reaches the end of the list,
+          the slow pointer will be at the middle node.
+        - Return the slow pointer as it points to the middle node.
+*/
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *slow = head, *fast = head;
+
+        while(fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+};
