41c5fb8c7b455e358841bdf48af594e798711080
Akashdip Neogi <Akashdipneogi@gmail.com>
2025-07-13 22:38:21 +0100

    pr no 33 added better explanation and solution

--- 33. Search in Rotated Sorted Array.cpp
+++ 33. Search in Rotated Sorted Array.cpp
@@ -9,31 +9,40 @@
 
     Explanation:
         - Comparing the middle element with the target.
-        - If the left half is sorted, check if the target is in that range.
-        - If the right half is sorted, check if the target is in that range.
+        - If the left half is sorted,
+            check if the target is in that range,
+                then adjust the right pointer with `mid - 1`.
+                else adjust the left pointer with `mid + 1`.
+        - If the right half is sorted,
+            check if the target is in that range.
+                then adjust the left pointer with `mid + 1`.
+                else adjust the right pointer with `mid - 1`.
         - Adjust the search range accordingly.
         - Return the index if found, otherwise return -1.
 */
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int mid = 0, begin = 0, end = nums.size() - 1;
-        while(begin <= end) {
-            mid = begin + (end - begin) / 2;
+        int mid = 0, left = 0, right = nums.size() - 1;
+
+        while(left <= right) {
+            mid = left + (right - left) / 2;
 
             if(nums[mid] == target)
                 return mid;
 
-            if(nums[begin] <= nums[mid])
-                if(target >= nums[begin] && target < nums[mid])
-                    end = mid - 1;
+            // Check if the left half is sorted
+            if(nums[left] <= nums[mid]){
+                if(target >= nums[left] && target < nums[mid])
+                    right = mid - 1;
                 else
-                    begin = mid + 1;
-            else
-                if(target > nums[mid] && target <= nums[end])
-                    begin = mid + 1;
+                    left = mid + 1;
+            } else {
+                if(target > nums[mid] && target <= nums[right])
+                    left = mid + 1;
                 else
-                    end = mid - 1;
+                    right = mid - 1;
+            }
         }
         return -1;
     }
