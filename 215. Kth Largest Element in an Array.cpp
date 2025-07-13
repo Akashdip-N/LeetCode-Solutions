7640d2eb8e29fe01c2d38ed9adb4cda693393fea
Akashdip Neogi <Akashdipneogi@gmail.com>
2025-07-13 23:17:21 +0100

    pr no 215 added better explanation and extra solution

--- 215. Kth Largest Element in an Array.cpp
+++ 215. Kth Largest Element in an Array.cpp
@@ -4,12 +4,15 @@
 /*
     Solution Approach: Using Priority Queue (Min Heap)
 
-    * Storing the values in the heap,
-        and checking if the size of the heap exceeds k,
-            If it does, we pop the smallest element from the heap.
-                because the question asks for the kth largest element.
+    Time Complexity: O(n log k)
+    Space Complexity: O(k)
+    n = size of the input array, k = the kth largest element to find
 
-    * Returning the top element of the heap as the kth largest element.
+    Explanation:
+        - Using min heap to keep track of the k largest elements.
+        - Pushing all the elements into the min heap,
+            and if the size of the heap exceeds k, we pop the smallest element.
+        - At the end, returing the top element of the min heap.
 */
 class Solution {
 public:
@@ -23,5 +26,29 @@ public:
         }
 
         return pq.top();
+    }
+};
+
+/**********************************************************************************/
+/*
+    Solution Approach: Using Sorting
+
+    Time Complexity: O(n log n)
+    Space Complexity: O(1)
+    n = size of the input array
+
+    Explanation:
+        - Sorting the array in ascending order.
+        - Returning the (n-k) th element of the sorted array.
+
+    NOTE:- Since we have sorted the array that means,
+            the largest element or the nth largest element will be last element,
+                hence the (n-k) th element will be the kth largest element.
+*/
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+       sort(nums.begin(),nums.end());
+       return nums[nums.size()-k];
     }
 };
