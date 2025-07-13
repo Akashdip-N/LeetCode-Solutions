8aeb6f9c9708a919806ebe2dc27104393ad4ecca
Akashdip Neogi <Akashdipneogi@gmail.com>
2025-07-13 15:41:14 +0100

    pr no 242 added better explanation and another solution

--- 242. Valid Anagram.cpp
+++ 242. Valid Anagram.cpp
@@ -2,14 +2,22 @@
     https://leetcode.com/problems/valid-anagram/
 */
 /*
-    Solution Approach:
-    * We are using a hash map to store the frequency of each character in the string s.
-    * Then we are iterating over the string t and
-        checking if the character is present in the hash map.
-    * If it is present, we are decrementing the frequency of the character.
-    * If the frequency of the character becomes 0,
-        we are removing the character from the hash map,
-            which means that the character is present in both the strings.
+    Solution Approach:- Using Hash Map
+
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+
+    Explanation:
+        - First storing all the characters of string s
+            in a hash map with their counts.
+        - Iterating through each character in string t:
+            - If the character exists in the map, we decrement its count.
+            - If the count reaches zero, we remove that character from the map,
+                because the number of occurrences of that character in t matches with s.
+            - If we find any character in t that does not exist in the map,
+                we return false.
+        - After the loop gets completed, we return true,
+            indicating that both strings are anagrams of each other.
 */
 class Solution {
 public:
@@ -31,5 +39,29 @@ public:
         }
 
         return true;
+    }
+};
+
+/**************************************************************************/
+/*
+    Solution Approach:- Using Sorting
+
+    Time Complexity: O(n log n)
+    Space Complexity: O(1)
+
+    Explanation:
+        - First we sort both strings s and t.
+        - If the sorted strings are equal, then they are anagrams of each other.
+*/
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size())
+            return false;
+
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        return s == t;
     }
 };
