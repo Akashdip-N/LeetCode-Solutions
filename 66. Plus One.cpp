c8f9dc92d9fdff7a638d13c41c48cc054673b75f
Akashdip Neogi <Akashdipneogi@gmail.com>
2025-07-22 00:19:27 +0100

    Solved pr no 66 added better explanation

--- 66. Plus One.cpp
+++ 66. Plus One.cpp
@@ -2,25 +2,36 @@
     https://leetcode.com/problems/plus-one/
 */
 /*
-    Solution Approach:-
+    Solution Approach:- Coming from end to the beginning
 
-    * Checking if the digit is 9 then replacing it with 0
-    * If we encounter any digit other than 9 just increment the value and return the array.
-    * Outside the loop, if the left-most digit is 0 then add 1 in front of it.
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+    n = size of the input vector
+
+    Explanation:
+        - We iterate from the last digit to the first digit.
+        - If the current digit is 9, we set it to 0 and continue to the next digit.
+        - If the current digit is not 9, we simply increment it by 1 and return the vector.
+        - If we finish the loop and all digits were 9,
+            we insert a 1 at the beginning and return the vector.
+        - At the end, we return the modified vector.
 */
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        for (int i = digits.size() - 1; i >= 0; i--)
+        for (int i = digits.size() - 1; i >= 0; i--) {
             if(digits[i] == 9)
                 digits[i] = 0;
             else{
                 ++digits[i];
                 return digits;
             }
+        }
 
+        // To handle cases where the digits are all 9
         if(digits[0] == 0)
             digits.insert(digits.begin(), 1);
+
         return digits;
     }
 };
