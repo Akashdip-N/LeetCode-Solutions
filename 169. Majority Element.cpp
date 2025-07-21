1e6fac666f88cbcf3fa9c2023156365bae2ac654
Akashdip Neogi <Akashdipneogi@gmail.com>
2025-07-22 00:10:32 +0100

    Solved pr no 169 added better explanation & solution

--- 169. Majority Element.cpp
+++ 169. Majority Element.cpp
@@ -2,22 +2,24 @@
     https://leetcode.com/problems/majority-element/
 */
 /*
-    Boyer-Moore Voting Algorithm Solution Approach:-
+    Solution Approach: Boyer-Moore Voting Algorithm
 
-    To implement this solution,
-    1. Initialize ans = nums[0] and freq = 0
-    2. Traverse the array
-        a. If freq == 0, which means the frequency of the current element is 0,
-            then update ans = nums[i]
-        b. If ans == nums[i], which means the current element is the majority element,
-            then increment freq
-        c. Else, decrement freq
-    3. Return ans
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+    n = size of the array
+
+    Explanation:
+        - We maintain a candidate for the majority element and a frequency counter.
+        - If the frequency counter is zero, we set the current element as the candidate.
+        - If the current element matches the candidate, we increment the frequency counter.
+        - If it does not match, we decrement the frequency counter.
+        - At the end of the traversal, the candidate will be the majority element.
 */
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int ans = nums[0], freq = 0;
+
         for (int i = 0; i < nums.size(); i++){
             if (freq == 0)
                 ans = nums[i];
@@ -33,36 +35,57 @@ public:
 };
 
 /*************************************************************************/
-
 /*
-    Map Implementation Solution Approach:-
+    Solution Approach: Using Sorting
 
-    To implement this solution:-
-    1. Initialize an unordered_map<int, int> mp
-    2. Traverse the array
-        a. If the element is not present in the map,
-            then add the element to the map with value 1
-        b. Else, increment the value of the element in the map
-        c. If the value of the element in the map is greater than n/2,
-            where n is the size of the array, then return the element
-    3. Return 0
+    Time Complexity: O(n log n)
+    Space Complexity: O(1)
+    n = size of the array
+
+    Explanation:
+        - We sort the array and return the middle element.
+        - The middle element will always be the majority element
+            since it appears more than n/2 times.
+*/
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return nums[nums.size() / 2];
+    }
+};
+
+/*************************************************************************/
+/*
+    Solution Approach: Using Hash Map
+
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+    n = size of the array
+
+    Explanation:
+        - Hash map to count and store the frequency of each element.
+        - For each element, we check if its frequency exceeds n/2.
+            Then we return the element as the majority element.
+        - If the array has only one element, we return that element.
 */
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         unordered_map<int, int> mp;
 
-        if(nums.size() == 1)
-            return nums[0];
+        if(nums.size() == 1) return nums[0];
 
-        for(auto num : nums)
+        for(auto num : nums) {
             if(mp.find(num) == mp.end())
                 mp[num] = 1;
             else{
                 mp[num]++;
+
                 if(mp[num] > nums.size()/2)
                     return num;
             }
+        }
 
         return 0;
     }
