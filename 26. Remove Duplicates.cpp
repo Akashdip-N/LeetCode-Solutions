8916bdc9d30e5911446fbb5be867b36d146b58a3
Akashdip Neogi <Akashdipneogi@gmail.com>
2025-07-21 12:43:12 +0100

    Pr no. 26 added better solution explanation

--- 26. Remove Duplicates.cpp
+++ 26. Remove Duplicates.cpp
@@ -2,18 +2,17 @@
     https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 */
 /*
-    Solution Approach: Two Pointers
-    Time Complexity: O(n), n = size of the input array
-    Space Complexity: O(1), in-place modification of the input array
+    Solution Approach: Two Pointers Approach
+
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+    n = size of the input array
 
     Explanation:
-        i. Store the position of the first element from the sorted array in `pos`.
-        ii. Create a variable `count` to keep track of the number of unique elements.
-        iii. Iterate through the array starting from the second element,
-                and check if the current element is different from the element at `pos`,
-                    if true then swap the current element with the element at `pos`,
-                        and increment the `count` of unique elements.
-        iv. Return the count of unique elements.
+        i. Bringing all the unique elements to the front of the array.
+        ii. Using `pos` to track the position of the last unique element.
+        iii. Incrementing `count` for each unique element found.
+        iv. The final count of unique elements is returned.
 */
 class Solution {
 public:
