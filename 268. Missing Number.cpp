26fb57de30a8d5b1e0380cc91575df727bb10d3e
Akashdip Neogi <Akashdipneogi@gmail.com>
2025-07-22 11:08:41 +0100

    Pr no 268 added different solutions & explanations

--- 268. Missing Number.cpp
+++ 268. Missing Number.cpp
@@ -2,40 +2,112 @@
     https://leetcode.com/problems/missing-number/
 */
 /*
-    Summation Solution Approach:-
+    Solution Approach:- Using Sum Formula
 
-    To implement this solution:-
-    1. Calculate the sum from 0 to n(size of the array)
-    2. Traverse the array
-        a. Subtract the current element from the sum
-    3. Return the sum, which will the missing number
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+    n = size of the array
+
+    Explanation:-
+        - We store the sum of all the elements from 0 to n in a variable `sum`.
+        - We then subtract each element in the array from `sum`.
+        - The final value of `sum` will be the missing number.
 */
-class Solution {                                // Time Complexity: O(n)
+class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int size = nums.size();
-        int sum = size * (size + 1) / 2;        // O(1)
+        int n = nums.size();
+        int sum = n * (n + 1) / 2;
+
         for(auto i : nums)
             sum -= i;
+
         return sum;
     }
 };
 
+/********************************************************************************/
 /*
-    Sorting Solution Approach:-
+    Solution Approach:- Using XOR
 
-    To implement this solution:-
-    1. Sort the array
-    2. Traverse the array
-        a. If the current element is not equal to the index,
-            then return the index
-    3. Return the size of the array
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+    n = size of the array
+
+    Explanation:-
+        - We use the XOR operation to find the missing number.
+        - We XOR all numbers from 0 to n and also XOR all elements in the array.
+        - The result will be the missing number
+            since XORing a number with itself cancels it out.
+*/
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int size = nums.size();
+        int xorSum = 0;
+
+        for(int i = 0; i <= size; i++)
+            xorSum ^= i;
+
+        for(auto num : nums)
+            xorSum ^= num;
+
+        return xorSum;
+    }
+};
+
+/********************************************************************************/
+/*
+    Solution Approach:- Using Hash Map
+
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+    n = size of the array
+
+    Explanation:-
+        - We use a hash map to store the presence of each number in the array.
+        - We then iterate from 0 to n and check if each number is present in the hash map.
+        - If a number is not found, we return it as the missing number.
+*/
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        unordered_map<int, bool> numSet;
+
+        for(int num: nums)
+            numSet[num] = true;
+
+        for(int i = 0; i <= nums.size(); i++) {
+            if(numSet.find(i) == numSet.end())
+                return i;
+        }
+
+        return -1;
+    }
+};
+
+/********************************************************************************/
+/*
+    Solution Approach:- Using Sorting
+
+    Time Complexity: O(n log n)
+    Space Complexity: O(1)
+    n = size of the array
+
+    Explanation:-
+        - First sorting the array.
+        - Iterating through the sorted array and
+            checking if the current index matches the value at that index.
+        - If it doesn't match, we return the index as the missing number.
+        - Outside the loop, we return the size of the array as the missing number
+            if all indices matched their values.
 */
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         int size = nums.size();
+
         for(int i = 0; i < size; i++)
             if(nums[i] != i)
                 return i;
