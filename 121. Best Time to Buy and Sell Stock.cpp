df391181c5efe509a0102dda6c405ae867db9bdb
Akashdip Neogi <Akashdipneogi@gmail.com>
2025-09-04 01:14:37 +0100

    Pr no 121 added better solution explanation

--- 121. Best Time to Buy and Sell Stock.cpp
+++ 121. Best Time to Buy and Sell Stock.cpp
@@ -2,20 +2,20 @@
     https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 */
 /*
-    Ex:- [7,1,5,3,6,4]
-    Ans:- 5
-    Explanation:- Buy at day 2 and sell at day 4, profit is 6 - 1 = 5
+    Solution Approach: Two Pointer Approach
 
-    Solution Approach:- Sliding Window Problem
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+    n = size of prices array
 
-    To implement this solution,
-    * Store the 1st day price as min and profit as 0
-    * Iterate over the array and check if the current price is less than min
-        * If yes, update min
-    * Else check if the profit is less than the current price - min
-        * If yes, update profit
-    * Else do nothing
-    * Return profit
+    Explanation:
+        i. Store the first element of prices array in min variable.
+        ii. Store 0 in profit variable.
+        iii. Iterate through the prices array starting from index 1.
+        iv. If the current element is less than min, update min to current element.
+        v. Else, if the difference between current element and
+            min is greater than profit, update profit to the difference.
+        vi. Finally, return profit.
 */
 class Solution {
 public:
@@ -23,7 +23,7 @@ public:
         int min = prices[0];
         int profit = 0;
 
-        for(int i = 1; i < prices.size(); i++){
+        for(int i = 1; i < prices.size(); i++) {
             if(min > prices[i])
                 min = prices[i];
             else
