b9fc478de1ae64df4f8948834002da561ba00199
Akashdip Neogi <Akashdipneogi@gmail.com>
2025-09-04 22:24:37 +0100

    Pr no 169 slight change in solution

--- 169. Majority Element.cpp
+++ 169. Majority Element.cpp
@@ -18,7 +18,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int ans = nums[0], freq = 0;
+        int ans = 0, freq = 0;
 
         for (int i = 0; i < nums.size(); i++){
             if (freq == 0)
