08aaae5e4b92ae6b2ebdeb0330350833f50d61cf
Akashdip Neogi <Akashdipneogi@gmail.com>
2025-09-12 20:04:55 +0100

    Pr no 01 added better solution explanation

--- 1. two sum.cpp
+++ 1. two sum.cpp
@@ -2,29 +2,36 @@
     https://leetcode.com/problems/two-sum/
 */
 /*
-    What we are doing in this solution is,
-    we are taking each number at a time and then checking if the remainder exists or not
-    in the question it is given that there is one solution only
-    that means there should be atleast one solution, hence after finding the combination
-    we are returining it.
+    Solution Approach:- Using Hash Map
+
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+    n = size of the input array
+
+    Explanation:-
+        i. Create an unordered map to store the elements and their indices.
+        ii. Iterate through the array and for each element, calculate the
+            complement (target - current element).
+        iii. Check if the complement exists in the map.
+            a. If it does, return the indices of the current element and the complement.
+            b. If it doesn't, add the current element and its index to the map.
+        iv. If no such pair is found, return an empty vector.
 */
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> ret;
         unordered_map<int, int> map;
-        // value, position
 
         for(int i = 0 ; i < nums.size(); i++){
             int rem = target - nums[i];
-            if(map.find(rem) != map.end()){
-                ret.push_back(map[rem]);
-                ret.push_back(i);
-                return ret;
-            }
+
+            if(map.find(rem) != map.end())
+                return {map[rem], i};
             else
                 map[nums[i]] = i;
         }
+
         return ret;
     }
 };
