ed787b311ab94da1071bd31b287f62d6e0ba1ae6
Akashdip Neogi <Akashdipneogi@gmail.com>
2025-09-27 21:35:10 +0100

    Pr no 125 added better solution explanation

--- 125. Valid Palindrome.cpp
+++ 125. Valid Palindrome.cpp
@@ -1,24 +1,38 @@
 /*
     https://leetcode.com/problems/valid-palindrome/
 */
-/**************************** TWO POINTER SOLUTION *************************/
 /*
-    Solution Approach:-
-    * Checking characters from both ends of the string.
-    * If they are not equal, return false.
-    * If they are equal, move the pointers towards the center
-        and return true.
+    Solution Approach: Using Two Pointers
+
+    Time Complexity: O(N)
+    Space Complexity: O(1)
+    N = length of the string
+
+    Explanation:
+        * Using two pointers,
+            one from the start of the string i.e. i = 0,
+                and the other from the end of the string i.e. j = s.size() - 1
+        * Move the pointers towards each other until they meet in the middle.
+        * If the characters at both pointers are not alphanumeric,
+            move the respective pointer.
+        * Checking if the characters are not equal,
+            then return false.
+        * If the characters are equal,
+            move both pointers towards each other.
+        * At the end, if all characters are equal,
+            return true.
 */
 class Solution {
 public:
     bool isPalindrome(string s) {
         int i = 0, j = s.size() - 1;
 
-        while(i < j){
+        while(i < j) {
             if(!isalnum(s[i])){
                 i++;
                 continue;
             }
+
             if(!isalnum(s[j])){
                 j--;
                 continue;
