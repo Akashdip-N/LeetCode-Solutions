b170d679c537e8f3d6e61c1a6cb96c2de13db3c7
Akashdip Neogi <Akashdipneogi@gmail.com>
2026-06-20 00:29:47 +0100

    Pr no 242 added better solution & explanation

--- 242. Valid Anagram.cpp
+++ 242. Valid Anagram.cpp
@@ -2,6 +2,39 @@
     https://leetcode.com/problems/valid-anagram/
 */
 /*
+    Solution Approach:- Using Hash-Map
+
+    Time Complexity:- O(N)
+    Space Complexity:- O(N)
+    N = length of the first string (s)
+
+    Explanation:-
+        - Storing the frequency of each character in the first and second
+            strings in two separate hash maps.
+        - Iterating through one of the strings and
+            checking if the frequency of each character in both hash maps is the same.
+        - If the frequency of any character is different, returning false.
+        - If all characters have the same frequency, returning true.
+*/
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+
+        unordered_map<char, int> m1;
+        unordered_map<char, int> m2;
+        for(auto c : s) m1[c]++;
+        for(auto ch : t) m2[ch]++;
+
+        for(auto c : s)
+            if(m1[c] != m2[c]) return false;
+
+        return true;
+    }
+};
+
+/***********************************************************************************/
+/*
     Solution Approach:- Using Hash Map
 
     Time Complexity: O(N)
@@ -16,7 +49,6 @@
         - If the count becomes zero, removing the character from the hash map.
         - Finally, if the hash map is empty, returning true, else false.
 */
-
 class Solution {
 public:
     bool isAnagram(string s, string t) {
@@ -54,7 +86,6 @@ public:
         - Sorting both the strings.
         - Compare the two sorted strings, and return the result.
 */
-
 class Solution {
 public:
     bool isAnagram(string s, string t) {
