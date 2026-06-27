cf809e759da1ebd5b858da040d7f1e6f759c67a9
Akashdip Neogi <Akashdipneogi@gmail.com>
2026-06-27 17:29:32 +0100

    Solved pr no 167

--- 167. Two Sum II - Input Array Is Sorted .cpp
+++ 167. Two Sum II - Input Array Is Sorted .cpp
@@ -2,7 +2,7 @@
     https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 */
 /*
-    Solution Approach:- Two Pointer with Binary Search
+    Solution Approach:- Using Two Pointers
 
     Time Complexity:- O(N)
     Space Complexity:- O(1)
@@ -27,6 +27,7 @@ public:
 
         while(left < right){
             int sum = numbers[left] + numbers[right];
+
             if(sum == target)
                 return {left + 1, right + 1};
             else
@@ -36,5 +37,45 @@ public:
                 right -= 1;
         }
         return res;
+    }
+};
+
+/*****************************************************************************/
+/*
+    Solution Approach:- Using Binary Search
+
+    Time Complexity:- O(NlogN)
+    Space Complexity:- O(1)
+    N = size of the input array
+
+    Explanation:-
+        i. First selecting an element from the array.
+        ii. Then using binary search to
+            find the other element from the array that sums up to the target.
+        iii. If found, return the indices of both elements.
+*/
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            int rem = target - nums[i];
+            int low = i + 1;
+            int high = n - 1;
+
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+
+                if (nums[mid] == rem)
+                    return {i + 1, mid + 1};
+                else if (nums[mid] > rem)
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+        }
+
+        return {};
     }
 };
