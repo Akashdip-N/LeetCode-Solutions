5119f3128484c701c2328458578fa16fe372fbfc
Akashdip Neogi <Akashdipneogi@gmail.com>
2026-07-24 00:06:14 +0100

    Pr no 139 added better solution explanation

--- 139. Word Break.cpp
+++ 139. Word Break.cpp
@@ -10,20 +10,90 @@
     M = Number of words in the wordDict
     K = Average length of the words in the wordDict
 
+    Intuition:-
+        * The idea is to reach the end of the string by checking the words from the wordDict.
+        * Using array to store the whether it's possible to reach to that specific index or not.
+        * At the end, we will return the value of the last index of the array,
+            that will tell whether it's possible to reach the end of the string or not.
+
     Explanation:
-        * Using a vector of boolean values and inialize it to false.
-        * The size of the vector is n + 1, where n is the length of the string s.
-        * Iterate through the string and for each position, check all words in the
-            dictionary to see if they can form a valid segment ending at that position.
-        * If a valid segment is found, mark the position as true in the dp array.
-        * Finally, return the value at dp[n], which indicates whether the entire
-            string can be segmented.
+        * Creating an array of string length + 1 and initializing it with false.
+        * Base case where we have an empty string, and setting the first index to true.
+        * Iterating through each index of the string and
+            checking if any of the words in the wordDict can be used to reach that index.
+        * If we can reach a index,
+            where the size of the word is less than or equal to the current window.
+        * Then we will check if whether it's possible to reach the index,
+            (i - length of the word) and
+                the substring in this window is equal to the word in the wordDict.
+        * If both conditions are true, then we can reach the current index and we will set
+            the value of that index to true and break the loop.
+
+        Ex:-
+            s = "applepenapple",
+            wordDict = ["apple","pen"]
+
+            dp = [true, false, false, false, false, false, false, false, false, false, false, false, false, false]
+
+            i.
+                i = 0,
+                current window size = a
+                No word in the wordDict can be used to reach this index.
+
+            .... goint to index i = 5
+
+            ii.
+                i = 5
+                current window size = apple
+                wordsize = 5
+                dp[5 - 5] = dp[0] = true
+                substring = s.substr(5 - 5, 5) = apple
+                word = apple
+
+                Both conditions are true, so we can reach this index
+                    and we will set dp[5] = true
+                        and break the loop.
+
+                dp = [true, false, false, false, false, true, false, false, false, false, false, false, false, false]
+
+            ... going to index i = 8
+
+            iii.
+                i = 8
+                current window size = applepen
+                wordsize = 3
+                dp[8 - 3] = dp[5] = true
+                substring = s.substr(8 - 3, 3) = pen
+                word = pen
+
+                Both conditions are true, so we can reach this index
+                    and we will set dp[8] = true
+                        and break the loop.
+
+                dp = [true, false, false, false, false, true, false, false, true, false, false, false, false, false]
+
+            ... going to index i = 13
+
+            iv.
+                i = 13
+                current window size = applepenapple
+                wordsize = 5
+                dp[13 - 5] = dp[8] = true
+                substring = s.substr(13 - 5, 5) = apple
+                word = apple
+
+                Both conditions are true, so we can reach this index
+                    and we will set dp[13] = true
+                        and break the loop.
+
+                dp = [true, false, false, false, false, true, false, false, true, false, false, false, false, true]
+
+            v. Returning dp[13] = true, which means we can reach the end of the string.
 */
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
         int n = s.size();
-
         vector<bool> dp(n + 1, false);
 
         // Base case: empty string can be segmented
