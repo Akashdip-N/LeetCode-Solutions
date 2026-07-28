c7bd6b19518f0152b5aa97a05ad122b1af1593d6
Akashdip Neogi <Akashdipneogi@gmail.com>
2026-07-28 17:49:39 +0100

    Pr no 3206 added better solution explanation

--- 3206. Alternating Groups I.cpp
+++ 3206. Alternating Groups I.cpp
@@ -2,19 +2,30 @@
     https://leetcode.com/problems/alternating-groups-i
 */
 /*
+    Solution Approach:- Using Sliding Window
+
     Time Complexity: O(N)
     Space Complexity: O(1)
     N = length of colors
 
+    Intuition:-
+        * We have to find a pair such that the 1st and the 3rd color are the same,
+            and the 2nd color is different from the 1st and the 3rd color.
+        * Using sliding window approach, we can find the count of such pairs in the colors array.
+        * Since the colors are positioned in a circular manner, the first and the last color,
+            are considered to be adjacent to each other.
+        * For this we have to iterate from the [1] position, till the [size + 2] position of the colors array.
+
     Explanation:
-        i. Using two pointers to represent the sliding window.
-        ii. Iterate through the colors array,
-            checking if the current color is the same as the previous color,
-                if they are the same, move the left pointer to the right.
-        iv. If the size of the window is greater than k,
-            move the left pointer to the right.
-        v. If the size of the window is equal to k, increment the count.
-        vi. Finally, return the count.
+        * Starting from the 1 position of the colors array,
+            we will check if the color before the current color is the same as the current color.
+        * If it is the same, we will move the left pointer to the current position.
+        * If the length of the window is greater than 3,
+            that means our window size is greater than 3,
+                we will move the left pointer to the right.
+        * If the length of the window is equal to 3,
+            we will increment the count of the pairs.
+        * Finally, we will return the count of the pairs
  */
 class Solution {
 public:
