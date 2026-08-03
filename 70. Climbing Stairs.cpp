2cd4bc4b2e6be5b5973cfae62539111b35edad4d
Akashdip Neogi <Akashdipneogi@gmail.com>
2026-08-03 13:00:31 +0100

    Pr no 70 added better solution explanation

--- 70. Climbing Stairs.cpp
+++ 70. Climbing Stairs.cpp
@@ -2,21 +2,33 @@
     https://leetcode.com/problems/climbing-stairs/
 */
 /*
-    Solution Approach: Top Down Approach with Memoization
+    Solution Approach: Using Dynamic Programming (Memoization)
 
     Time Complexity: O(N)
     Space Complexity: O(N)
     N = number of stairs
 
-    Explanation:
-        i. Start from the nth stair and recursively calculate the number of ways to
-            reach the top by taking either 1 or 2 steps.
-        ii. Using the base cases where if n is 0 or 1, there is only one way to reach the top.
-        iii. Use a map to store the results of previously calculated stairs
-            to avoid redundant calculations.
-        iv. Finally, return the number of ways to reach the nth stair.
-*/
+    Intuition:-
+        - We have to find the best way we can reach the nth stair.
+        - We can do this by taking either 1 or 2 steps at a time.
+        - Using dynamic programming and memoization approach
+            to store the previously calculated number of ways to reach the stairs.
 
+    Explanation:
+        * Using a helper function to compute the number of ways with which we can reach.
+        * Inside the helper function we are performing the following operation:-
+            i. Base case:-
+                If n is 0 or 1 (that means we are in the first or second stair), return 1.
+            ii. Checking if the number is already stored in the memo map,
+                if yes then return the stored value.
+            iii. Storing the number by calling the helper function recursively
+                for n-1 and n-2 and adding the two values to
+                    get the number of ways to reach the nth stair.
+            iv. Finally returning the number of ways to reach the nth stair from the memo map.
+
+        * In the main function we are calling the helper function
+            using a hashmap to store the number of ways to reach the stairs.
+*/
 class Solution {
 public:
     int countStairs(int n, map<int, int>& memo) {
@@ -38,26 +50,36 @@ public:
 
 /**********************************************************************************************/
 /*
-    Solution Approach: Bottom Up Approach with Tabulation
+    Solution Approach: Bottom up Dynamic Programming
 
     Time Complexity: O(N)
     Space Complexity: O(1)
     N = number of stairs
 
-    Explanation:
-        i. Using three variables to store the number of ways to reach the current stair,
-           the previous stair, and the stair before the previous stair.
-        ii. Initialize all three variables to 1.
-        iii. Iterate from 0 to n-1, updating the variables accordingly.
-        iv. Finally, return the variable that stores the number of ways to reach the nth stair.
-*/
+    Intuition:-
+        - The number of ways to reach the nth stair will always be 1
+        - And the number of ways to reach the (n-1)th stair will always be 1
+        - The number of ways to reach to the nth stair from the (n-1)th stair will be 1,
+            because if we take 2 steps it would be out of bound.
+        - So we would calculate from the end (n - 2) to the start (0) and
+            find the number of ways to reach the nth stair.
+        - This is similar to the Fibonacci sequence.
 
+    Explanation:
+        * Using 3 variables:-
+            i. a = number of ways to reach the (n-1)th stair
+            ii. b = number of ways to reach the (n-2)th stair
+            iii. c = number of ways to reach the nth stair
+        * Starting from the 2nd last stair and going to the 0th stair,
+            we will calculate the number of ways to reach the nth stair.
+        * At the end we will return the number of ways to reach the nth stair.
+*/
 class Solution {
 public:
     int climbStairs(int n) {
         int a = 1, b = 1, c = 1;
 
-        for(int i = 0; i < (n-1); i++) {
+        for(int i = (n - 2); i >= 0; i--) {
             c = a;
             a += b;
             b = c;
