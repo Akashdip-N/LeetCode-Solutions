f035d70eb75f4d297b99efebbdd6c8af887e374f
Akashdip Neogi <Akashdipneogi@gmail.com>
2026-08-03 12:32:23 +0100

    Pr no 509 added better solution explanation

--- 509.Fibonacci Number.cpp
+++ 509.Fibonacci Number.cpp
@@ -4,14 +4,24 @@
 /*
     Solution Approach:- Iterative approach
 
-    Time Complexity: O(n)
+    Time Complexity: O(N)
     Space Complexity: O(1)
-    n = number of elements in the Fibonacci sequence
+    N = number of elements in the Fibonacci sequence
 
-    Explanation:
-        - Use two variables to store the last two Fibonacci numbers.
-        - Iterate from 2 to n, updating the variables to compute the next Fibonacci number.
-        - Return the nth Fibonacci number.
+    Intuition:-
+        * Fibonacci sequence is all about finding the sum of the earlier two numbers
+            for the current number.
+        * Using the earlier two number and storing the sum of the current number in a variable.
+            and updating the earlier numbers for the next iteration.
+
+    Explanation:-
+        * Initialize two variables a and b with 0 and 1 respectively.
+        * Iterating from 2 to n, while do so performing the following steps:
+            - Calculate the sum of a and b and store it in c.
+            - Update a with the value of b.
+            - Update b with the value of c.
+        * At the end we check if n is 0 then we return a(0)
+            otherwise we return b (the nth Fibonacci number).
 */
 class Solution {
 public:
@@ -30,19 +40,29 @@ public:
 
 /*****************************************************************************************/
 /*
-    Solution Approach:- Recursive approach with memoization
+    Solution Approach:- Using Dynamic Programming (Memoization)
 
-    Time Complexity: O(n)
-    Space Complexity: O(n)
-    n = number of elements in the Fibonacci sequence
+    Time Complexity: O(N)
+    Space Complexity: O(N)
+    N = number of elements in the Fibonacci sequence
+
+    Intuition:
+        - Since we are using recursive method to calculate the Fibonacci number.
+        - Storing the previously computed Fibonacci numbers in a vector to avoid recomputation.
+        - At the end returning the nth Fibonacci number from the memo vector.
 
     Explanation:
-        - Use a helper function to compute the Fibonacci number recursively.
-        - Use a vector to store previously computed Fibonacci numbers (memoization).
-        - If the Fibonacci number for n is already computed,
-            return it from the memo vector.
-        - Otherwise, compute it recursively and store the result in the memo vector.
-        - Return the memoized nth value.
+        * Using a helper function to compute the Fibonacci number recursively.
+            - Inside the helper function we are checking the following:-
+                i. Base case:- If n is less than or equal to 1, return n (i.e., 0 or 1).
+                ii. Checking if the number is already stored in the memo vector,
+                    if yes then return the stored value.
+                iii. Storing the number by calling the helper function recursively
+                    for n-1 and n-2 and returning the sum.
+                iv. At the end returning the nth Fibonacci number from the memo vector.
+        * In the main funciton
+            - Initializing a vector of size (n + 1) with -1 to store the numbers
+            - Calling the helper function with n and the memo vector.
 */
 class Solution {
 public:
@@ -67,17 +87,12 @@ public:
 /*
     Solution Approach:- Tail recursive approach
 
-    Time Complexity: O(n)
-    Space Complexity: O(n)
-    n = number of elements in the Fibonacci sequence
+    Time Complexity: O(N)
+    Space Complexity: O(N) (due to recursion stack)
+    N = number of elements in the Fibonacci sequence
 
-    Explanation:
-        - Use a helper function to compute the Fibonacci number using tail recursion.
-        - The helper function takes two parameters (a and b) representing
-            the last two Fibonacci numbers, a count of how many numbers have been computed,
-            and the target n.
-        - If the count equals n, return a (the nth Fibonacci number).
-        - Otherwise, recursively call the helper function with updated values.
+    Intuition:
+        - This solution is the combination between the iterative and recursive approach.
 */
 class Solution {
 public:
@@ -103,17 +118,21 @@ public:
 /*
     Solution Approach:- Recursive approach without memoization
 
-    Time Complexity: O(2^n)
-    Space Complexity: O(n)
-    n = number of elements in the Fibonacci sequence
+    Time Complexity: O(2^N)
+    Space Complexity: O(N) (due to recursion stack)
+    N = number of elements in the Fibonacci sequence
 
-    Explanation:
-        - Use a recursive function to compute the Fibonacci number.
-        - Base cases:
-            - If n is 0, return 0.
-            - If n is 1 or 2, return 1.
-        - Recursively call the function for n-1 and n-2 and return their sum.
-        - This approach has exponential time complexity due to repeated calculations.
+    Intuition:-
+        - The problem requires the sum of the previous two numbers to find the current number.
+        - We call the same function recursively for n-1 and n-2 until we reach the base case.
+        - Then we return the sum of the two numbers to get the nth Fibonacci number.
+
+    Explanation:-
+        * Base case:-
+            i. If n is 0, return 0
+            ii. If n is 1 or 2, return 1.
+        * Otherwise,
+            return the sum of fib(n - 1) and fib(n - 2) to get the nth Fibonacci number.
 */
 class Solution {
 public:
