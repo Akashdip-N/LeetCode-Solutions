bbfc4f476be2dccb2d56271391f0195add37b26b
Akashdip Neogi <Akashdipneogi@gmail.com>
2026-08-20 00:32:20 +0100

    Pr no 11 added better solution explanation

--- 11. Container With Most Water.cpp
+++ 11. Container With Most Water.cpp
@@ -2,22 +2,34 @@
     https://leetcode.com/problems/container-with-most-water/
 */
 /*
-    Problem:- Using two pointers.
+    Solution Approach:- Using Two Pointer Approach
 
-    Time Complexity: O(n)
+    Time Complexity: O(N)
     Space Complexity: O(1)
-    n = length of the input array
+    N = length of the input array
+
+    Intuition:-
+        - We have to find the area with maximum water that can be contained between two lines.
+        - We would start from both ends of the array and calulate the area formed by the two lines.
+        - Each time we would check the and store the area of the current two lines,
+            and the current maximum area.
+        - Then we would move the pointer based on the height of the line.
+        - At the end we would return the maximum area found.
 
     Explanation:
-        - Two pointers are initialized at the start and end of the array.
-        - Run a loop until the two pointers meet.
-        - First calculate the area formed by the two pointers.
-        - Update the maximum area if the current area is larger.
-        - Check if the
-            left pointer's height is less than the right pointer's height,
-                if so, increment the left pointer.
-        - Else decrement the right pointer.
-        - Return the maximum area found.
+        * Initializing left to 0 and right to the last index of the array.
+        * Initializing max_area to 0.
+        * While left is less than right:
+            - Calculating the area formed by the two lines at left and right.
+            - Updating max_area if the current area is greater than max_area.
+            - If the height of the line at left is less than the height of the line at right,
+                - Incrementing left pointer to move to the next line.
+            - Else, decrementing right pointer to move to the previous line.
+            (
+                This is done because we want to maximize the area,
+                    and the area is limited by the shorter line.
+            )
+        * Returning max_area.
 */
 class Solution {
 public:
