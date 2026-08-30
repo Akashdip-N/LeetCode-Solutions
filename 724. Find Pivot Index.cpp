c8bccb0643a4ad628e0d21996c1591edc04975cb
Akashdip Neogi <Akashdipneogi@gmail.com>
2026-08-30 23:26:51 +0100

    Pr no 724 added better solution explanation

--- 724. Find Pivot Index.cpp
+++ 724. Find Pivot Index.cpp
@@ -8,14 +8,27 @@
     Space Complexity: O(1)
     N = length of the array
 
+    Intuition:-
+        - We need to find the pivot index in the array such that,
+            the sum of the number to the left of the index
+                is equal to the sum of the numbers to the right of the index.
+        - So first we store the sum of the entire array in a variable.
+        - Iterate of the array to find the pivot index.
+        - If none index is found, we return -1.
+
     Explanation:-
-        * The pivot index is the index where the sum of the numbers to the left is equal to the sum of the numbers to the right.
-        * We can calculate the total sum of the array and then iterate through the array,
-            keeping track of the left sum and calculating the
-                right sum as total_sum - left_sum - nums[i].
-        * If at any index, left_sum equals right_sum,
-                we return that index as the pivot index.
-        * If no such index is found, we return -1.
+        * Base case:- If the array is an empty array, we return -1.
+
+        * Storing the sum of the entire array in a variable.
+        * Iterating through the entire array from index 0 til the end of the array,
+            and performing the following steps:-
+                - First we find the right sum by
+                    subtracting the left sum and the current number from the total sum.
+                - Then checking if the left sum is equal to the right sum.
+                    - If yes, we return the current index as the pivot index.
+                - If not, we add the current number to the left sum and
+                    continue to the next index.
+        * If we reach the end of the array and no pivot index is found, we return -1.
  */
 class Solution {
 public:
