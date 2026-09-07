108db283442b5c1bcd66dc6a0599c97d5883fa66
Akashdip Neogi <Akashdipneogi@gmail.com>
2026-09-07 17:39:55 +0100

    Solved Pr no 1268

---
+++ 1268. Search Suggestions System.cpp
@@ -0,0 +1,93 @@
+/*
+    https://leetcode.com/problems/search-suggestions-system/
+*/
+/*
+    Solution Approach:- Using Sorting and Two Pointers
+
+    Time Complexity:- O(NlogN + M) (Because each time we would put only 3 suggestions in the result)
+    Space Complexity:- O(M * L)
+    N = number of products / words in the products list
+    M = length of the searchWord
+    L = average length of the words in the products list
+
+    Intuition:-
+        - Given us a list of product names and a search word.
+        - We need to return a list of lists of suggested products after each character
+            of the search word is typed.
+        - If there are more than three products that match the search word,
+            return the three lexicographically minimums products.
+
+            Example:-
+                    products = ["mobile","mouse","moneypot","monitor","mousepad"]
+                    searchWord = "mouse"
+                    Output: [
+                        ["mobile","moneypot","monitor"], => After typing 'm'
+                        ["mobile","moneypot","monitor"], => After typing 'mo'
+                        ["mouse","mousepad"],            => After typing 'mou'
+                        ["mouse","mousepad"],            => After typing 'mous'
+                        ["mouse","mousepad"]             => After typing 'mouse'
+                    ]
+
+        - Since, we need the products in lexicographical order, we can sort the products list.
+        - Then, instead of searching the entire products list for each character,
+            we would use two pointer to check the range of products that matches the search word
+                and then change the range of the search space after each character is typed.
+
+            Ex:-
+                products = ["mobile","mouse","moneypot","monitor","mousepad"]
+                searchWord = "mouse"
+
+                - When typed 'm', the range of products that matches is
+                    [0, 4] => ["mobile","mouse","moneypot","monitor","mousepad"]
+                - When typed 'mo', the range of products that matches is
+                    [0, 4] => ["mobile","mouse","moneypot","monitor","mousepad"]
+                - When typed 'mou', the range of products that matches is
+                    [1, 4] => ["mouse","mousepad"]
+                - When typed 'mous', the range of products that matches is
+                    [1, 4] => ["mouse","mousepad"]
+                - When typed 'mouse', the range of products that matches is
+                    [1, 4] => ["mouse","mousepad"]
+
+    Explanation:-
+        * Sorting the products list in lexicographical order.
+        * Setting the left pointer to index 0 and
+            right pointer to the last index of the products list.
+        * For each character in the search word,
+            - Checking if the current character in the left
+            and right pointers matches the current character in the search word.
+            - If it doesn't match, we move the left pointer to the right and
+                the right pointer to the left until we find a match.
+            - After that, we add the products in the range of left and right pointers
+                to the current suggestions list.
+            - Finally, we add the current suggestions list to the result list.
+        * Returning the result list after all characters in the search word are processed.
+*/
+class Solution {
+public:
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        sort(products.begin(), products.end());
+        vector<vector<string>> result;
+
+        int left = 0;
+        int right = products.size() - 1;
+
+        for (int i = 0; i < searchWord.size(); i++) {
+            char c = searchWord[i];
+
+            while (left <= right && (products[left].size() <= i || products[left][i] != c))
+                left++;
+
+
+            while (left <= right && (products[right].size() <= i || products[right][i] != c))
+                right--;
+
+            vector<string> currentSuggestions;
+            for (int j = 0; j < 3 && left + j <= right; j++)
+                currentSuggestions.push_back(products[left + j]);
+
+            result.push_back(currentSuggestions);
+        }
+
+        return result;
+    }
+};
