44a301a96016306fa4d0e2e046f86c2bdf87630d
Akashdip Neogi <Akashdipneogi@gmail.com>
2026-09-17 12:53:06 +0100

    Pr no 703 added better solution explanation

--- 703. Kth Largest Element in a Stream.cpp
+++ 703. Kth Largest Element in a Stream.cpp
@@ -2,20 +2,43 @@
     https://leetcode.com/problems/kth-largest-element-in-a-stream
 */
 /*
-    Solution Approach:- Using Priority Queue in the form of Min Heap
+    Solution Approach:- Using Priority Queue
 
-    * While pushing the value into the heap,
-        check if the current size of the heap exceeds the maximum size,
-            because in the question it is mentioned that
-                we need to find the kth largest element.
-                    so we do not need more than k elements in the heap.
+    Time Complexity:- O(N logK)
+    Space Complexity:- O(K)
+    N = Number of elements in the stream
+    K = Kth largest element
 
-    * Check if the value of the top element in the heap is less than the
-        value being pushed into the heap
-            if so, which means that the value being pushed is
-                greater than the kth largest element
-                    but we don't want to do that
-                        so we pop the value first then push the new value
+    Intuition:-
+        - We have to implement the following:-
+            1. KthLargest(int k, vector<int>& nums) -
+                Initializes the object with the integer k and the stream of integers nums.
+            2. int add(int val) -
+                Appends the integer val to the stream and
+                returns the element representing the kth largest element in the stream.
+
+        - Using a min-heap (priority queue) to keep track of the k largest elements in the stream.
+
+    Explanation:-
+        * Creating a min-heap (priority queue) to store the k largest elements in the stream.
+        * And creating a variable to store the size of the min-heap (priority queue).
+
+        KthLargest Operation:-
+            * Takes an integer k and a vector of integers nums as input.
+            * Initializes the size of the min-heap (priority queue) to k.
+            * Iterating through each elements in the vector and
+                calling the add function and passing the element as an argument.
+
+        Add Operation:-
+            * Takes an integer val as input.
+            * Firsr checking if the size of the min-heap (priority queue) is less than k,
+                then we push the element into the min-heap (priority queue).
+            * Else if the size of the min-heap (priority queue) is equal to k
+                then first we check
+                    if the top element in the priority queue is less than the top element
+                If true, then we pop the top element from the min-heap (priority queue) and
+                push the new element into the min-heap (priority queue).
+            * Finally we return the top element of the min-heap (priority queue).
 */
 class KthLargest {
     private:
@@ -31,11 +54,11 @@ class KthLargest {
         int add(int val) {
             if(pq.size() < max_size)
                 pq.push(val);
-            else
-                if(val > pq.top()){
-                    pq.pop();
-                    pq.push(val);
-                }
+            else if(val > pq.top()) {
+                pq.pop();
+                pq.push(val);
+            }
+
             return pq.top();
         }
 };
